2eb60b3db5a9c5ec0fa9a4b39efd1ad846c079c7
Wittmann <ssh.wtmn@gmail.com>
2024-01-25 17:18:52 +0200

    2nd commit. Add 2nd SOUT in the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello new GIT test");
+
+        System.out.println("2nd commit master branch");
     }
 }
